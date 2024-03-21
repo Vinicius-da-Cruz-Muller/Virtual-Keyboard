6ac24d2b69590d908b78bb980cdd1b9abfc566dc
Vinicius-da-Cruz-Muller <108363297+Vinicius-da-Cruz-Muller@users.noreply.github.com>
2024-03-21 12:24:52 -0300

    primeira versão estável do teclado virtual

--- project/static/app.js
+++ project/static/app.js
@@ -1,15 +1,18 @@
 const buttons = document.querySelectorAll('.btn');
 // const textarea = document.querySelector('textarea')
-const senhaInput = document.getElementById('senha');
-
+const senhaInput = document.getElementById('pass');
+const btnLogin = document.getElementById('btn_login');
 const delete_btn = document.querySelector('.delete');
+
 let passwordChars = [];
 
 buttons.forEach(btn =>{
     btn.addEventListener('click', ()=>{
         passwordChars.push(btn.innerText);
         senhaInput.value = passwordChars.join('');
+        senhaInput.dispatchEvent(new Event('input'));
         console.log(passwordChars)
+        console.log(senhaInput.value)
     });
 });
 
